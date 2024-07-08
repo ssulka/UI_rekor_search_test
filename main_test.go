c46dc1a09aced2dcbe2a3d83b941038aed05198f
ssulka <samtheskiller@gmail.com>
2024-07-08 15:34:47 +0200

    works until choosing email option

--- main_test.go
+++ main_test.go
@@ -16,15 +16,15 @@ type G struct {
 
 var setup = func() func(t *testing.T) G {
 	return func(t *testing.T) G {
-		browser := rod.New().MustConnect() // Create a new browser instance for each test
-		t.Parallel()                       // run each test concurrently
+		browser := rod.New().Headless(false).MustConnect() // create a new browser instance for each test
+		t.Parallel()                                       // run each test concurrently
 		return G{got.New(t), browser}
 	}
 }
 
 // a helper function to create an incognito page.
 func (g G) page(url string) *rod.Page {
-	page := g.browser.MustIncognito().MustPage(url)
+	page := g.browser.MustPage(url)
 	g.Cleanup(func() {
 		if err := page.Close(); err != nil {
 			g.Logf("Failed to close page: %v", err)
@@ -33,32 +33,44 @@ func (g G) page(url string) *rod.Page {
 	return page
 }
 
-const appURL = "http://localhost:3000"
+const appURL = "http://localhost:3000" //will be replaced by URL from ocp
 
 // test for email
 func TestEmail(t *testing.T) {
 	fmt.Println("came here0")
-	g := setup()(t) // Invoke setup to get a new instance of G for this test
+	g := setup()(t) // invoke setup to get a new instance of G for this test
 	fmt.Println("came here1")
 	p := g.page(appURL)
 	fmt.Println("came here2")
-	// Ensure the element is ready before interacting with it
+	// ensure the element is ready before interacting with it
 	attrElement := p.MustElement("#rekor-search-attribute")
 	attrElement.MustWaitVisible().MustClick()
+
 	fmt.Println("came here3")
 
-	// Select the "email" option from the dropdown
+	//select the "email" option from the dropdown
+	// emailOption := p.MustElementR("option", "Email")
+	// emailOption.MustWaitVisible().MustClick()
+	// fmt.Println("Finding 'Email' option")
 	emailOption := p.MustElementR("option", "Email")
-	emailOption.MustWaitVisible().MustClick()
+
+	fmt.Println("'Email' option found")
+	emailOption.MustWaitVisible()
+	fmt.Println("'Email' option is visible")
+	emailOption.MustClick()
+	fmt.Println("'Email' option clicked")
 	fmt.Println("came here4")
 
-	// Fill the text field with the email "jdoe@redhat.com"
+	// fill the text field with the email "jdoe@redhat.com"
 	emailInput := p.MustElement("#rekor-search-email")
 	emailInput.MustWaitVisible().MustInput("jdoe@redhat.com")
 	fmt.Println("came here5")
 
-	// Verify the input value
+	// verify the input value
 	inputValue := emailInput.MustProperty("value").String()
 	g.Eq(inputValue, "jdoe@redhat.com")
 	fmt.Println("came here6")
+
+	searchButton := p.MustElement("search-form-button")
+	searchButton.MustClick()
 }
